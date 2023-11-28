649fc6792310f5c858fc9f51bd048bf93ebf0539
mohsin-islam2024 <152082206+mohsin-islam2024@users.noreply.github.com>
2023-11-28 18:22:09 +0600

    I have upload my first portfolio

---
+++ script.js
@@ -0,0 +1,27 @@
+// var mohsin = [1, 2, 3, 4];
+
+// var pen = mohsin[2, 3];
+// console.log(pen);
+
+// mohsin.unshift(10, 55);
+// console.log(mohsin.length);
+// mohsin.pop();
+// mohsin.pop();
+// mohsin[3];
+// console.log(mohsin);
+
+// var maruf = {
+//     name: "maruf",
+//     age: 21,
+//     study: "honors"
+// };
+
+// maruf.name = "rakib"
+
+// console.log(maruf.name);
+// console.log(maruf.age);
+var a = "i am"
+var b = " mohsin"
+var c = a.concat(b);
+var d = c.substring(8, 2)
+console.log(d);
